097649bd82782d09c39c13fb5e25304c076a1070
mojvaf <mvafa44@gmail.com>
2020-09-15 12:30:35 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronRight,
+  faChevronLeft,
+  faCircle,
+  faCheckCircle,
+  faPlus
+} from "@fortawesome/free-solid-svg-icons";
+
+const Main = () => {
+  const [items, setItem] = useState([
+    { itemName: "item 1", quantity: 1, isSelected: false },
+    { itemName: "item 2", quantity: 3, isSelected: false },
+    { itemName: "item 3", quantity: 2, isSelected: true }
+  ]);
+
+  const [inputValue, setInputValue] = useState("");
+  const [totalItemCount, setTotalItemCount] = useState(6);
+
+  const handleAddButtonClick = () => {
+    const newItem = {
+      itemName: inputValue,
+      quantity: 1,
+      isSelected: false
+    };
+
+    const newItems = [...items, newItem];
+    setItem(newItems);
+    setInputValue("");
+  };
+
+  const handleQuantityIncrease = index => {
+    const newItems = [...items];
+
+    newItems[index].quantity++;
+
+    setItem(newItems);
+    calculateTotal();
+  };
+
+  const handleQuantityDecrease = index => {
+    const newItems = [...items];
+
+    newItems[index].quantity--;
+
+    setItem(newItems);
+    calculateTotal();
+  };
+
+  const toggleComplete = index => {
+    const newItems = [...items];
+
+    newItems[index].isSelected = !newItems[index].isSelected;
+
+    setItem(newItems);
+  };
+
+  const calculateTotal = () => {
+    const totalItemCount = items.reduce((total, item) => {
+      return total + item.quantity;
+    }, 0);
+    setTotalItemCount(totalItemCount);
+  };
+
+  return (
+    <div className="main-container">
+      <div className="add-item-box">
+        <input
+          value={inputValue}
+          onChange={event => setInputValue(event.target.value)}
+          className="add-item-input"
+          placeholder="Add an item..."
+        />
+        <FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
+      </div>
+      <div className="item-list">
+        {items.map((item, index) => (
+          <div className="item-container">
+            <div className="item-name" onClick={() => toggleComplete(index)}>
+              {item.isSelected ? (
+                <>
+                  <FontAwesomeIcon icon={faCheckCircle} />
+                  <span className="completed">{item.itemName}</span>
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faCircle} />
+                  <span>{item.itemName}</span>
+                </>
+              )}
+            </div>
+            <div className="quantity">
+              <button>
+                <FontAwesomeIcon
+                  icon={faChevronLeft}
+                  onClick={() => handleQuantityIncrease(index)}
+                />
+              </button>
+              <span>{item.quantity}</span>
+              <button>
+                <FontAwesomeIcon
+                  icon={faChevronRight}
+                  onClick={() => handleQuantityDecrease(index)}
+                />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        <div className="total">Total: {totalItemCount}</div>
+      </div>
+    </div>
+  );
+};
+export default Main;
